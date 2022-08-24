214621f0234e81cf2932be6d1bd6ccaa172cee57
Vitor Ian <goncalvesvitorian@gmail.com>
2022-08-23 22:52:37 -0300

    responsivo e arquivos base

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,59 @@
-var light = document.getElementById("light");
-var dark = document.getElementById("dark");
-var element = document.body;
+function btnVer() {
+    const btnVer = document.getElementById("ver-mais");
 
-light.onclick = function (){
-    if (document.getElementById("theme-light").checked) {
-        element.classList.toggle("light");
-      } else{
-        element.classList.remove("light");
+    if(btnVer.ariaExpanded == "true"){
+        btnVer.innerHTML="Ver Menos";
+    }else{
+        btnVer.innerHTML="Ver Mais";
     }
-}
+};
 
-dark.onclick = function (){
-    if (document.getElementById("theme-light").checked) {
-        element.classList.toggle("light");
-      } else{
-        element.classList.remove("light");
+function saveIdPage(){
+    const idPage = localStorage.getItem("last_page");
+    let lastVisit =JSON.parse(localStorage.getItem('last_visit'));
+    if(lastVisit == null){
+        lastVisit = ['mmc','mdc','reg3','tt','5','6','7','8'];
     }
+    if(lastVisit.indexOf(idPage) < 0){
+        if(idPage !== null){
+            lastVisit.unshift(idPage);
+            lastVisit.pop();
+        }
+        localStorage.setItem('last_visit', JSON.stringify(lastVisit));
+    }
+
+    acessadosRecente();
 }
 
-var scroll = document.getElementById("scroll");
-var condensed = document.getElementById("condensed");
-var menu = document.getElementById("menu-lat");
+function acessadosRecente(){
+    const itens =JSON.parse(localStorage.getItem('last_visit'));
+    const matematica = ['mmc','mdc'];
+    const geradores = ['tt','ss'];
+    const outros = ['reg3','cc'];
+    let categoria = "";
+    let icon = "";
 
-scroll.onclick = function (){
-  if (document.getElementById("menu-condensed").checked) {
-      menu.classList.toggle("condesed-menu");
-    } else{
-      menu.classList.remove("condesed-menu");
-  }
-}
+    $(itens).each(function(index, value){
+        //verifica a categoria
+        if(matematica.indexOf(value) > -1){
+            categoria = "matematica";
+            icon = "<div class='icon-item icon-mat mb-4'><i class='bi bi-calculator-fill'></i></div>";
+        } else if(geradores.indexOf(value) > -1){
+            categoria = "geradores";
+            icon = "<div class='icon-item icon-ger mb-4'><i class='bi bi-hammer'></i></div>";
+        } else if(outros.indexOf(value) > -1){
+            categoria = "outros";
+            icon = "<div class='icon-item icon-out mb-4'><i class='bi bi-grid-fill'></i></div>";
+        }else{
+            console.log("arruma logo essa lista");
+            icon = "<div class='icon-item icon-out mb-4'><i class='bi bi-question-octagon-fill'></i></div>";
+        }
 
-condensed.onclick = function (){
-  if (document.getElementById("menu-condensed").checked) {
-      menu.classList.toggle("condesed-menu");
-    } else{
-      menu.classList.remove("condesed-menu");
-  }
-}
-
-var defaultTheme = document.getElementById("default-theme");
-defaultTheme.onclick = function (){
-  element.classList.remove("light");
-  menu.classList.remove("condesed-menu");
+        if(index < 4){
+            $("<a class='item-dash item-ac-rc' href='./" + categoria + "/" + value + ".html'>" + icon + "<span class='title-item'>" + value + "</span></a>").appendTo("#ac-rc-1");
+            
+        }else{
+            $("<a class='item-dash item-ac-rc' href='./" + categoria + "/" + value + ".html'>" + icon + "<span class='title-item'>" + value + "</span></a>").appendTo("#ac-rc-2");
+        }
+    });
 }
